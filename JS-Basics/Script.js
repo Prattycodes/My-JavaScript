6abafb7f0058bfe62611ef3bdb845dced0d206da
Pratham Anil Tetgure <89979888+Prattycodes@users.noreply.github.com>
2023-12-10 19:38:12 +0530

    Basics-JS-VideoNo1

---
+++ JS-Basics/Script.js
@@ -0,0 +1,79 @@
+//Word vs Keyword 
+// Word Examples- Chacha, Rahul, Boy, etc.
+// Keyword Examples- var, let, etc.
+
+//Variables and constant 
+// const dulha = "Lab";
+// const dulhan= "Labby";
+
+// Hoisting 
+// console.log(b);
+// var b= 12;
+// console.log(a);
+
+//Types in JS 
+//Primitive Type 
+// var a= 28928;
+// var b= "PRatham";
+//Reference 
+// var a= [1, 2, 3, 4];
+// var b = {};
+
+//Conditionals- If else, else if
+// if(True){
+//     console.log("Hello");
+// }
+// else{
+//     console.log("No Hello");
+// }
+
+//Loops - for while
+// for(start; end ; change){
+
+// }
+// for(var i= 0; i<11; i++){
+//     console.log(i);
+// }
+// var a=1;
+// while(a<10){
+//     console.log(a);
+//     a++;
+// }
+// console.log(a)
+
+//Functions
+// function fun(){
+//     console.log("Funny");
+// }
+// fun();
+
+// function fun(a, b, c){ //params 
+//     console.log(a, b, c);
+// }
+// fun(1, 2, 3); //Arguments 
+
+//Arrays 
+// var arr= [1, 2, 3, 4, 5];
+// console.log(arr[3]);
+
+//Push pop unshift shift splice 
+// var arr= [1, 2, 3, 4, 5];
+// arr.push(6);
+// arr.pop();
+// arr.unshift(0);
+// arr.shift();
+// arr.splice(2, 2);
+
+//Object - ek bande ki saari baat ki toh object 
+// 1) Empty Object 
+var a= {
+
+};
+// 2) Filled Object 
+var b ={
+    age: 26, 
+    name: "Pratham", 
+    city: "Dombivali", //All above are properties 
+    talk: function(){} //Method
+};
+b.age= 20;
